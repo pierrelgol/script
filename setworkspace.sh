b0111fde2c07df2508e83fbee2f8f702380bbc2e
Samaritain56 <119697612+Samaritain56@users.noreply.github.com>
2023-03-30 12:37:35 +0100

    Update setworkspace.sh

--- setworkspace.sh
+++ setworkspace.sh
@@ -25,7 +25,7 @@ source $HOME/.cargo/env
 
 # Create the workspace directory
 mkdir -p ~/workspace
-
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 # Clone NvChad and open Neovim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
